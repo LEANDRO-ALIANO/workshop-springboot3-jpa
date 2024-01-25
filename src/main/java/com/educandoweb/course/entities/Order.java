05f56a991aee48a0800936b0678322d281448dca
leandro aliano <alianoleandro0610@outlook.com>
2024-01-25 14:06:49 -0300

    OrderStatus ENUM

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,16 +27,19 @@ private static final long serialVersionUID = 1L;
 private Long id;
 private Instant moment;
 
+private Integer orderStatus;
+
 @ManyToOne
 @JoinColumn(name= "client_id")
 private User client;
 
 public Order() {}
 
-public Order(Long id, Instant moment,  User client) {
+public Order(Long id, Instant moment,OrderStatus orderStatus,  User client) {
 	super();
 	this.id = id;
 	this.moment = moment;
+	setOrderStatus(orderStatus);
 	this.client = client;
 }
 
@@ -69,6 +74,15 @@ public Instant getMoment() {
 
 public void setMoment(Instant moment) {
 	this.moment = moment;
+}
+
+
+public OrderStatus getOrderStatus() {
+	return OrderStatus.valueOf(orderStatus);
+}
+
+public void setOrderStatus(OrderStatus orderStatus) {
+	this.orderStatus = orderStatus.getCode();
 }
 
 public User getClient() {
