1616030c008742ae0698fa012b166868b737030e
leandro aliano <alianoleandro0610@outlook.com>
2024-01-26 01:26:39 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,23 +1,35 @@
 package com.educandoweb.course.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
-public class Category {
+@Table(name = "tb_category")
+public class Category implements Serializable{
+/**
+	 * 
+	 */
+private static final long serialVersionUID = 1L;
+
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 private String name;
-
-//private List<Product> products= new ArrayList<>();
+@JsonIgnore
+@ManyToMany(mappedBy = "categories")
+private Set<Product> products= new HashSet<>();
 
 public Category() {
 	
@@ -48,6 +60,12 @@ public String getName() {
 
 public void setName(String name) {
 	this.name = name;
+}
+
+
+
+public Set<Product> getProducts() {
+	return products;
 }
 
 
--- src/main/java/com/educandoweb/course/resources/CategoryResourse.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResourse.java
@@ -13,7 +13,7 @@ import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.services.CategoryService;
 
 @RestController
-@RequestMapping(value="category")
+@RequestMapping(value="categories")
 /**
  * estou informando que é um controlador rest
  */
