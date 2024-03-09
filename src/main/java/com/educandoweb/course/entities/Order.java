c9a89a252fb0d8f4ece6de9aaf35cf87a9dbebf0
Leandro <alianoleandro0610@outlook.com>
2024-03-09 14:05:08 -0300

    Product OrderItem one-To-Many

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,17 +2,13 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 
 @Entity
@@ -32,6 +28,8 @@ private Integer orderStatus;
 @ManyToOne
 @JoinColumn(name= "client_id")
 private User client;
+@OneToMany(mappedBy = "id.order")
+private Set<OrderItem> items = new HashSet<>();
 
 public Order() {}
 
@@ -93,4 +91,7 @@ public void setClient(User client) {
 	this.client = client;
 }
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 }
